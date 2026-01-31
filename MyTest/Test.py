ad88b2d0884f72bd3207508502cf9eb66959eb8a
AlexandrKolyada <alexsandrkoljada@gmail.com>
2026-01-31 13:31:23 +0100

    gitignore

---
+++ MyTest/Test.py
@@ -0,0 +1,16 @@
+#my_dict = {'name': 'Василь', 'age': 25, 'city': 'Київ'}
+
+#for key in my_dict:
+   # print(key)
+#print("|"*50)
+#for value in my_dict.values():
+   # print(value)
+#print("|"*50)
+#for key, value in my_dict.items():
+   # print(key, value)
+age = 8
+
+for i in range(age):
+    for j in range(age):
+        print(f'{i}{j}', end= ' ')
+    print()
