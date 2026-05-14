59178ac453722f680ac16393c10665436e96cae8
AlexandrKolyada <alexsandrkoljada@gmail.com>
2026-05-14 19:29:53 +0200

    new fix

--- lesson_29/app/db_manager.py
+++ lesson_29/app/db_manager.py
@@ -1,20 +1,15 @@
-import psycopg2
+import psycopg
+
 
 class DBManager:
 
     def __init__(self, host, database, user, password, port=5432):
-        self.conn_params = {
-            "host": host,
-            "database": database,
-            "user": user,
-            "password": password,
-            "port": port
-        }
+        self.conn_string = f"host={host} dbname={database} user={user} password={password} port={port}"
         self.connection = None
 
     def connect(self):
         if not self.connection or self.connection.closed:
-            self.connection = psycopg2.connect(**self.conn_params)
+            self.connection = psycopg.connect(self.conn_string)
         return self.connection
 
     def close(self):
